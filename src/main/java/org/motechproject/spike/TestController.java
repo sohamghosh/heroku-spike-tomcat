b337d43b4c57c49d6476eefc311c3a96c15866bd
Soham Ghosh <sohamghosh@gmail.com>
2013-05-20 12:03:53 +0530

    Heroku Tomcat spike.

---
+++ src/main/java/org/motechproject/spike/TestController.java
@@ -0,0 +1,15 @@
+package org.motechproject.spike;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller@RequestMapping("/")
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping("/status")
+    private String create() {
+        return "Complimenti, si sta lavorando!";
+    }
+}
