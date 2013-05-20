548f66313a271bb5c63b84d1ef5b5d0b141e277a
Soham Ghosh <sohamghosh@gmail.com>
2013-05-20 15:50:48 +0530

    Added date in the status message.

--- src/main/java/org/motechproject/spike/TestController.java
+++ src/main/java/org/motechproject/spike/TestController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+
 @Controller@RequestMapping("/")
 public class TestController {
 
     @ResponseBody
     @RequestMapping("/status")
     private String status() {
-        return "Complimenti, si sta lavorando!";
+        return "Complimenti, si sta lavorando! " + new Date();
     }
 }
