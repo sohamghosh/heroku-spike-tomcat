aed914dc8140c6c0185fba18eb761bc3289aedef
Soham Ghosh <sohamghosh@gmail.com>
2013-05-21 20:00:14 +0530

    Moved protocols out of the repository.

--- src/main/java/org/motechproject/spike/TestController.java
+++ src/main/java/org/motechproject/spike/TestController.java
@@ -1,13 +1,9 @@
 package org.motechproject.spike;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
 import java.util.Date;
 
 @Controller
@@ -18,24 +14,5 @@ public class TestController {
     @RequestMapping("/status")
     private String status() {
         return "Complimenti, si sta lavorando! " + new Date();
-    }
-
-    @ResponseBody
-    @RequestMapping("/http")
-    private String http() throws IOException {
-        HttpClient client = new HttpClient();
-        HttpMethod method = new GetMethod("http://www.google.com");
-        final int responseCode = client.executeMethod(method);
-        return "Status code: " + responseCode;
-    }
-
-    @ResponseBody
-    @RequestMapping("/smpp")
-    private String smpp() {
-        return "SMPP";
-    }
-
-    public static void main(String[] args) throws IOException {
-        System.out.println(new TestController().http());
     }
 }
